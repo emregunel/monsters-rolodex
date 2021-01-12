30382ccd4a84c4c82826e0fcef8d88273e02f61d
Emre Gunel <emrgunel@gmail.com>
2021-01-12 19:38:32 +0100

    adding files for gh pages

---
+++ src/App.js
@@ -0,0 +1,60 @@
+import React, { Component } from 'react';
+
+import {CardList} from './components/card-list/card-list.component'
+import {SearchBox} from './components/search-box/search-box.component'
+
+import './App.css';
+
+
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      monsters: [
+        {
+          name:'Frankenstein',
+          id:'asc1'
+        },{
+          name:'Dracula',
+          id:'asc2'
+        },{
+          name:'Zombie',
+          id:'asc3'
+        }
+      ],
+      searchField:''
+    }
+    this.handleChange=this.handleChange.bind(this);
+  }
+
+  componentDidMount(){
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(res=>{ return res.json()})
+    .then(users=>this.setState({monsters:users}))
+    .catch()
+  }
+
+  handleChange= (e)=>{
+    this.setState({searchField:e.target.value});
+  }
+
+  render() {
+    const { monsters,searchField } = this.state;
+    const filteredMonsters=monsters.filter(monster=>
+      monster.name.toLowerCase().includes(searchField.toLowerCase())
+      );
+    return (
+      <div className='App'>
+        <h1>Monsters Rolodex</h1>
+        <SearchBox placeholder='search monsters' handleChange={this.handleChange}/>
+
+        <CardList monsters={filteredMonsters}>
+        </CardList>
+
+      </div>
+    );
+  }
+}
+
+export default App;
